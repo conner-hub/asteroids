ff61f38e4c6195d5c25f3fa5747922803c065f78
conner-hub <connerartss@gmail.com>
2025-07-28 17:42:47 -0400

    Screen functionality.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import pygame
+from constants import *
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+    print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameactive = True
+    while gameactive == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0), rect=None)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
